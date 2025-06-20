bf30a587856ce2f744a9e0b42f9c1cf0c12a1f2f
User <astakamokro@yandex.ru>
2025-06-20 20:49:18 +0300

    json form of project page

--- src/routes/ProjectPage.jsx
+++ src/routes/ProjectPage.jsx
@@ -1,5 +1,7 @@
 import {useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
+import {StaticTreeDataProvider, Tree, UncontrolledTreeEnvironment} from "react-complex-tree";
+
 
 
 function ProjectPage(){
@@ -43,8 +45,41 @@ function ProjectPage(){
         return <p>Error: {error.message}</p>;
     }
 
-    // data manipulation
-    console.log(data);
+
+
+    const items = {
+        root: {
+            index: 'root',
+            canMove: true,
+            isFolder: true,
+            children: ['child1', 'child2'],
+            data: 'Root item',
+            canRename: true,
+        },
+        child1: {
+            index: 'child1',
+            canMove: true,
+            isFolder: false,
+            children: [],
+            data: 'Child item 1',
+            canRename: true,
+        },
+        child2: {
+            index: 'child2',
+            canMove: true,
+            isFolder: false,
+            children: ["child3"],
+            data: 'Child item 2',
+            canRename: true,
+        },
+
+
+    };
+
+    const dataProvider = new StaticTreeDataProvider(items, (item, newName) =>
+        ({ ...item, data: newName }));
+
+
 
 
 
@@ -54,6 +89,16 @@ function ProjectPage(){
             <p>{user_name} ! Your project {data.name} has opened!</p>
 
             <p>{JSON.stringify(data.root)}</p>
+
+            <UncontrolledTreeEnvironment
+                dataProvider={dataProvider}
+                getItemTitle={item => item.data}
+                viewState={{}}
+            >
+                <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+            </UncontrolledTreeEnvironment>
+
+
 
 
         </div>
